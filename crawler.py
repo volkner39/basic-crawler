8b70e7ada061a1e5745cf2bc7d5ea8a825e682b4
volkner39 <kezaram.tharmasegaram@mail.utoronto.ca>
2021-01-27 02:01:59 -0500

    fixed linux bugs

--- crawler.py
+++ crawler.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import httplib2
 import urllib
@@ -26,42 +27,43 @@ def rec(current_url, url):
 
 	try:
 		# get all links on current page
-		for link in BeautifulSoup(response, parse_only=SoupStrainer('a')):
+		for link in BeautifulSoup(response, 'html.parser', parse_only=SoupStrainer('a')):
 			if link.has_attr('href'):
-			
+
 				if ("#comment" not in link['href']) and ("#respond" not in link['href']):
-			
+
 					# remove the ending '/' to prevent duplicates
 					link['href'] = link['href'].rstrip('/')
-			
+
 					# absolute links
 					if link['href'][:4] == 'http':
 						if (url not in link['href']):
 							external_links.add(link['href'])
 						else:
 							main_urls.append(link['href'])
-					
+
 					# relative links
 					elif (len(link['href']) > 1) and (link['href'][0] == '/'):
 						# join main url with relative link
 						new_url = urljoin(current_url, link['href'])
 						main_urls.append(new_url)
-				
+
 					# weird links
 					else:
-					
-						if "#comment" not in current_url:
+						if len(link['href']) > 0 and link['href'][0] == '#':
 							main_urls.append(urljoin(current_url, link['href']))
-									
+						else:
+							weird_links.add(link['href'])
+
 	except TypeError:
 		print("Undefined coding")
-		
+
 
 if __name__ == "__main__":
-	
+
 	start_time = time.time()
 	url = sys.argv[1]
-	
+
 	# first call to original link
 	rec(url.rstrip('/'), url.rstrip('/'))
 	s[url] = 1
@@ -74,31 +76,35 @@ if __name__ == "__main__":
 		# check for duplicate links
 		if (main_urls[i] not in s.keys()):
 			rec(main_urls[i], url.rstrip('/'))
-			
+
 			# set link as checked in dict
 			s[main_urls[i]] = 1
 			print(main_urls[i])
 		i += 1
-	
-	
+
+
 	# write all links found to a text file
-	f = open("main_urls" + ' - ' + url + ".txt", "w")
-	f1 = open("externals" + ' - ' + url + ".txt", "w")
-	f2 = open("weird" + ' - ' + url + ".txt", "w")
-	
-	for x in sorted(s):
-		f.write(x + '\n')
-	
-	f.close()
-	
-	for y in sorted(external_links):
-		f1.write(y + '\n')
-	
-	f1.close()
-	
-	for z in sorted(weird_links):
-		f2.write(z + '\n')
-		
-	f2.close()
-	
+	folder = os.path.dirname(os.path.abspath(__file__))
+	file1 = os.path.join(folder, 'main_urls.txt')
+	file2 = os.path.join(folder, 'externals.txt')
+	file3 = os.path.join(folder, 'weird.txt')
+
+	with open(file1, "w+") as f:
+		for x in sorted(s):
+			f.write(x + '\n')
+
+		f.close()
+
+	with open(file2, "w+") as f1:
+		for y in sorted(external_links):
+			f1.write(y + '\n')
+
+		f1.close()
+
+	with open(file3, "w+") as f2:
+		for z in sorted(weird_links):
+			f2.write(z + '\n')
+
+		f2.close()
+
 	print("--- %s seconds ---" % (time.time() - start_time))
